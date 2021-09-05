5820626ed2fc119f86733fb2e5ac1d79e416707a
Vichislav <s1ava8968@gmail.com>
2021-09-05 21:00:55 +0600

    new part for css

--- html_css_js/3_Flexbox/js/pizza.js
+++ html_css_js/3_Flexbox/js/pizza.js
@@ -4,21 +4,25 @@ let currentSize;
 var btn25 = document.getElementById("btn25");
 var btn30 = document.getElementById("btn30");
 var btn35 = document.getElementById("btn35");
-document.getElementById('size').innerHTML = (0 +" см");
+
+
+function outputsize(currentSize) {
+    document.getElementById('size').innerHTML = (currentSize +" см");
+}
 
 btn25.onclick = function () {
     currentSize = 25;
     console.log(currentSize + "см");
-    document.getElementById('size').innerHTML = (currentSize+ " см");
+    outputsize(currentSize);
 }
 btn30.onclick = function () {
     currentSize = 30;
     console.log(currentSize + "см");
-    document.getElementById('size').innerHTML = (currentSize+ " см");
+    outputsize(currentSize);
 }
 btn35.onclick = function () {
     currentSize = 35;
     console.log(currentSize + "см");
-    document.getElementById('size').innerHTML = (currentSize+ " см");
+    outputsize(currentSize);
 }
 

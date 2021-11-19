7fe165ecd41d41df5f931494ed99d24d12480f7d
Svalyan <you@example.com>
2021-11-19 18:36:17 +0600

    new while

--- Java/IntelliJ IDEA/equation/src/equation/Main.java
+++ Java/IntelliJ IDEA/equation/src/equation/Main.java
@@ -11,20 +11,47 @@ public class Main  {
 
     public static void main(String[] args) throws IOException
     { //throws IOException что за ошибки?
-        double a;
-        double b;
-        double c;
+        double a = 0.0;
+        double b = 0.0;
+        double c = 0.0;
         String continueChoice;
 
         do {
-            System.out.println("Введите коэффициент [a] уравнения типа ax^2 + bx + c");
-            a = Integer.parseInt(reader.readLine());
 
-            System.out.println("Введите коэффициент [b] уравнения типа ax^2 + bx + c");
-            b = Integer.parseInt(reader.readLine());
+            boolean isValidA = false;
+            boolean isValidB = false;
+            boolean isValidC = false;
 
-            System.out.println("Введите коэффициент [c] уравнения типа ax^2 + bx + c");
-            c = Integer.parseInt(reader.readLine());
+            while(isValidA == false)
+            {
+                System.out.println("Введите коэффициент [a] уравнения типа ax^2 + bx + c");
+                try {
+                    a = Integer.parseInt(reader.readLine());
+                    isValidA = true;
+                } catch (NumberFormatException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            while(isValidB == false)
+            {
+                System.out.println("Введите коэффициент [b] уравнения типа ax^2 + bx + c");
+                try {
+                    b = Integer.parseInt(reader.readLine());
+                    isValidB = true;
+                } catch (NumberFormatException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            while(isValidC == false)
+            {
+                System.out.println("Введите коэффициент [c] уравнения типа ax^2 + bx + c");
+                try {
+                    c = Integer.parseInt(reader.readLine());
+                    isValidC = true;
+                } catch (NumberFormatException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
 
             System.out.print("Ваше уравнение имеет вид? " + a + "*x^2+" + b + "x+" + c + " Y/N:");
 
@@ -32,9 +59,11 @@ public class Main  {
         } while (continueChoice.equalsIgnoreCase("N"));
 
         ArrayList<Double> root = Solver.SolveEquation(a, b, c);
-        // вывод с помощью массива + забор данных из файла
-        for (int i = 0; i < root.size(); i++)
-            System.out.printf("корень №" + (i + 1) + " " + root.get(i) + "\n" );
+        if (root == null)
+            System.out.println("Уравнение не имеет корней");
+        else
+            for (int i = 0; i < root.size(); i++)
+                System.out.printf("корень №" + (i + 1) + " " + root.get(i) + "\n" );
     }
 }
 

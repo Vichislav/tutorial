c46cc25fa81a89320204db61d7ed706348817525
Svalyan <you@example.com>
2021-12-01 21:41:28 +0600

    refactoring

--- Java/IntelliJ IDEA/ExcelReader/src/reader/Main.java
+++ Java/IntelliJ IDEA/ExcelReader/src/reader/Main.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         try {
-            ParsingExcel.EqualExcelFiles("test.xls", "test_1.xls", 20, 20);
+            ParsingExcel.EqualExcelFiles("test.xls", "test_1.xls");
         } catch (IOException exception) {
             System.out.println(exception.getMessage());
         }
--- Java/IntelliJ IDEA/ExcelReader/src/reader/ParsingExcel.java
+++ Java/IntelliJ IDEA/ExcelReader/src/reader/ParsingExcel.java
@@ -2,45 +2,48 @@ package reader;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Objects;
+
 
 public final class ParsingExcel {
-    public static void EqualExcelFiles(String filePath1, String filePath2, int quantityRow, int quantityCell) throws IOException {
+    public static void EqualExcelFiles(String filePath1, String filePath2) throws IOException {
 
         FileInputStream fileInputStreamA = new FileInputStream(filePath1);
-        Workbook workA = new HSSFWorkbook(fileInputStreamA);
+        Workbook workbookA = new HSSFWorkbook(fileInputStreamA);
+
+
 
         FileInputStream fileInputStreamB = new FileInputStream(filePath2);
-        Workbook workB = new HSSFWorkbook(fileInputStreamB);
+        Workbook workbookB = new HSSFWorkbook(fileInputStreamB);
 
         FileOutputStream fileOutputStreamB = new FileOutputStream(filePath2);
 
-        CellStyle newStyle = workB.createCellStyle();
+        CellStyle newStyle = workbookB.createCellStyle();
         newStyle.setFillForegroundColor(IndexedColors.GREY_40_PERCENT.getIndex());
         newStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
+        int quantityRow = workbookA.getSheetAt(0).getPhysicalNumberOfRows();
+
         for (int rowCount = 0; rowCount < quantityRow; rowCount++) {
-            for (int cellCount = 0; cellCount < quantityCell; cellCount++) {
+            for (int cellCount = 0; cellCount < workbookA.getSheetAt(0).getRow(rowCount).getPhysicalNumberOfCells(); cellCount++) {
                 try {
-                    Cell cell0 = workA.getSheetAt(0).getRow(rowCount).getCell(cellCount);
+                    Cell cell0 = workbookA.getSheetAt(0).getRow(rowCount).getCell(cellCount);
                     String result0 = ParsingExcel.ParsingCell(cell0);
 
-                    Cell cell1 = workB.getSheetAt(0).getRow(rowCount).getCell(cellCount);
+                    Cell cell1 = workbookB.getSheetAt(0).getRow(rowCount).getCell(cellCount);
                     String result1 = ParsingExcel.ParsingCell(cell1);
 
                     if (!result0.equals(result1))
                         cell1.setCellStyle(newStyle);
-                } catch (NullPointerException e) {
-                   System.out.println(e.getMessage());
-                }
-            } // for cellCount
-        } // for rowCount
 
-        workB.write(fileOutputStreamB);
+                } catch (NullPointerException e) {
+                   //System.out.println(e.getMessage());
+                }
+            }
+        }
+        workbookB.write(fileOutputStreamB);
 
         fileInputStreamA.close();
         fileInputStreamB.close();

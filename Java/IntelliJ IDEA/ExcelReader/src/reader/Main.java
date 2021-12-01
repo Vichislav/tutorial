864b3b0a3dc6dbe2304dfb4dc4a6303661145fa7
Svalyan <you@example.com>
2021-12-01 20:17:47 +0600

    refactoring 2

--- Java/IntelliJ IDEA/ExcelReader/src/reader/Main.java
+++ Java/IntelliJ IDEA/ExcelReader/src/reader/Main.java
@@ -1,151 +1,17 @@
 package reader;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.usermodel.HSSFWorkbookFactory;
-import org.apache.poi.ss.usermodel.*;
-
 import java.io.*;
-import java.util.Objects;
-import java.util.function.DoubleBinaryOperator;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import static org.apache.poi.ss.usermodel.CellType.*;
-import static org.apache.poi.ss.usermodel.CellType.NUMERIC;
-import static org.apache.poi.ss.usermodel.CellType.STRING;
 
 public class Main {
 
-    //private static final Logger LOG = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) throws IOException {
-
-        //System.setProperty("log4j.configurationFile","./log4j2.xml");
-        //Logger log = LogManager.getLogger(Main.class.getName());
-
-        /*Workbook wb = new HSSFWorkbook(); // содание экз. класса HSSFWorkbook, ссылка на него это wb
-        Sheet sheet0 = wb.createSheet("НСТ"); // создание листа
-        Row row = sheet0.createRow(3); // "создание/объявление" строки 3
-        Cell cell = row.createCell(3); // "создание/объявление" ячейки 3 в строке 3
-        cell.setCellValue("НСТ");
-
-        Sheet sheet1 = wb.createSheet("АДМ");
-        Sheet sheet2 = wb.createSheet("ПГС");
-        Sheet sheet3 = wb.createSheet("ИСУ");
-
-
-        FileOutputStream fos = new FileOutputStream("MyBook.xls"); //создаем поток для работы с файлом...
-
-        wb.write(fos);
-        fos.close(); */ // поработали с потоком, закрыли поток
-
-        //********************пробуем читать из файла********************//
-
-        FileInputStream streamA = new FileInputStream("test.xls");
-        Workbook workA = new HSSFWorkbook(streamA);
-
-        FileInputStream streamB = new FileInputStream("test_1.xls");
-        Workbook workB = new HSSFWorkbook(streamB);
-
-        CellStyle newStyle = workB.createCellStyle();
-        //style.setFillBackgroundColor(IndexedColors.RED.getIndex());
-        newStyle.setFillPattern(FillPatternType.LESS_DOTS);
-
-
-        for (int rowCount = 0; rowCount < 17; rowCount++)
-        {
-            for (int cellCount = 0; cellCount < 17; cellCount++)
-            {
-                try {
-                    System.out.println("_" + cellCount + "_" + rowCount);
-
-                    //String result0 = workA.getSheetAt(0).getRow(row).getCell(cell).getStringCellValue(); // получаем данные 1 добавить метод getCellText(Cell cell)
-                    String result0 = "";
-                    Sheet sheet0 = workA.getSheetAt(0);
-                    Row row0 = sheet0.getRow(rowCount);
-                    Cell cell0 = row0.getCell(cellCount);
-
-                    switch (cell0.getCellType()) {
-                        case STRING:
-                            result0 = cell0.getRichStringCellValue().getString();
-                            break;
-                        case NUMERIC:
-                            if (DateUtil.isCellDateFormatted(cell0)) {
-                                result0 = cell0.getDateCellValue().toString();
-                            } else {
-                                result0 = Double.toString(cell0.getNumericCellValue());
-                            }
-                            break;
-                        case BOOLEAN:
-                            result0 = Boolean.toString(cell0.getBooleanCellValue());
-                            break;
-                        case FORMULA:
-                            result0 = cell0.getCellFormula().toString();
-                            break;
-                        default:
-                            break;
-                    }
-
-                    System.out.println(result0);
-
-                    //String result1 = workB.getSheetAt(0).getRow(rowCount).getCell(cellCount).getStringCellValue(); // получаем данные 2
-                    String result1 = "";
-                    Sheet sheet1 = workB.getSheetAt(0);
-                    Row row1 = sheet1.getRow(rowCount);
-                    Cell cell1 = row1.getCell(cellCount);
-
-                    switch (cell1.getCellType()) {
-                        case STRING:
-                            result1 = cell1.getRichStringCellValue().getString();
-                            break;
-                        case NUMERIC:
-                            if (DateUtil.isCellDateFormatted(cell1)) {
-                                result1 = cell1.getDateCellValue().toString();
-                            } else {
-                                result1 = Double.toString(cell1.getNumericCellValue());
-                            }
-                            break;
-                        case BOOLEAN:
-                            result1 = Boolean.toString(cell1.getBooleanCellValue());
-                            break;
-                        case FORMULA:
-                            result1 = cell1.getCellFormula().toString();
-                            break;
-                        default:
-                            break;
-                    }
-                    System.out.println(result1);
-
-
-                    if (!Objects.equals(result0, new String(result1))) //!result0.equals(result1)
-                    {
-                        // придаем текущей ячейке новый стиль
-//                        Sheet sheet2 = workB.getSheetAt(0);
-//                        Row row2 = sheet2.getRow(rowCount);
-//                        Cell cell2 = row2.getCell(cellCount);
-                        cell1.setCellStyle(newStyle);
-                        FileOutputStream writeStream = new FileOutputStream("test_1.xls");
-                        workB.write(writeStream);
-                        writeStream.close();
-                    }
-
-                } // block try
-                catch (NullPointerException e) {
-                    System.out.println(e.getMessage());
-                }
-            } // for cellCount
-        } // for rowCount
-
-        streamA.close();
-        streamB.close();
-
-
-
-
-
+    public static void main(String[] args) {
+        try {
+            ParsingExcel.EqualExcelFiles("test.xls", "test_1.xls", 20, 20);
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
--- Java/IntelliJ IDEA/ExcelReader/src/reader/ParsingExcel.java
+++ Java/IntelliJ IDEA/ExcelReader/src/reader/ParsingExcel.java
@@ -1,9 +1,52 @@
 package reader;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Objects;
 
 public final class ParsingExcel {
+    public static void EqualExcelFiles(String filePath1, String filePath2, int quantityRow, int quantityCell) throws IOException {
+
+        FileInputStream fileInputStreamA = new FileInputStream(filePath1);
+        Workbook workA = new HSSFWorkbook(fileInputStreamA);
+
+        FileInputStream fileInputStreamB = new FileInputStream(filePath2);
+        Workbook workB = new HSSFWorkbook(fileInputStreamB);
+
+        FileOutputStream fileOutputStreamB = new FileOutputStream("test_1.xls");
+
+        CellStyle newStyle = workB.createCellStyle();
+        newStyle.setFillForegroundColor(IndexedColors.GREY_40_PERCENT.getIndex());
+        newStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        for (int rowCount = 0; rowCount < quantityRow; rowCount++) {
+            for (int cellCount = 0; cellCount < quantityCell; cellCount++) {
+                try {
+                    Cell cell0 = workA.getSheetAt(0).getRow(rowCount).getCell(cellCount);
+                    String result0 = ParsingExcel.ParsingCell(cell0);
+
+                    Cell cell1 = workB.getSheetAt(0).getRow(rowCount).getCell(cellCount);
+                    String result1 = ParsingExcel.ParsingCell(cell1);
+
+                    if (!result0.equals(result1))
+                        cell1.setCellStyle(newStyle);
+                } catch (NullPointerException e) {
+                   System.out.println(e.getMessage());
+                }
+            } // for cellCount
+        } // for rowCount
+
+        workB.write(fileOutputStreamB);
+
+        fileInputStreamA.close();
+        fileInputStreamB.close();
+        fileOutputStreamB.close();
+    }
+
     public static String ParsingCell(Cell cell) {
         String result = "";
         switch (cell.getCellType()) {
@@ -30,4 +73,4 @@ public final class ParsingExcel {
         return result;
     }
 }
-public
+

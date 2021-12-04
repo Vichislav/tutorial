3ac12eefa38cdcb20894a2c0ed7ca3625c3fe6c7
Svalyan <you@example.com>
2021-12-04 16:17:29 +0600

    add new style, new controller

---
+++ Java/IntelliJ IDEA/demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button authSigInButton;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private Button loginSignUpButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}

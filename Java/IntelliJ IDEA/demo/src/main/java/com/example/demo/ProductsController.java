fdf3ceee6caccf8598a3590801a2a1866fe9aacd
Svalyan <you@example.com>
2021-12-22 18:49:17 +0600

    refactoring

--- Java/IntelliJ IDEA/demo/src/main/java/com/example/demo/ProductsController.java
+++ Java/IntelliJ IDEA/demo/src/main/java/com/example/demo/ProductsController.java
@@ -4,7 +4,9 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.beans.Observable;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
@@ -15,6 +17,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 public class ProductsController {
+
 
     @FXML
     private ResourceBundle resources;
@@ -67,31 +70,49 @@ public class ProductsController {
     @FXML
     private Button add;
 
+   /* ObservableList<Products> list = FXCollections.observableArrayList(
+            new Products(1, "Roll", 3, 1200, "OAO" )
+    );*/
+
+    //кнопка add добавить строку
+   @FXML
+   private void clickAdd (ActionEvent event) {
+       System.out.println("add work"); // проверка
+       //через конструктор класса Products создаем экземпляр класса и загоняем туда значение из окон input
+       Products products = new Products(
+               Integer.parseInt(inputId.getText()),       // id
+               inputName.getText(),                       // Name
+               Double.parseDouble(inputAmount.getText()), // Amount
+               Double.parseDouble(inputPrice.getText()),  // Price
+               inputSupplier.getText());                  // Supplier
+       ObservableList<Products> products1 = productsTable.getItems();
+       products1.add(products);
+       productsTable.setItems(products1);
+   }
+
+   //кнопка remove удалить строку
+    @FXML
+    private void clickRemove (ActionEvent event) {
+        System.out.println("remove work"); // проверка
+        int selectedID = productsTable.getSelectionModel().getSelectedIndex();
+        productsTable.getItems().remove(selectedID);
+    }
+
+
 
 
     @FXML
-    void initialize(URL url, ResourceBundle resourceBundle) {
-        //
+    void initialize (URL url, ResourceBundle resourceBundle) {
+        System.out.println("initialize work");
+
+
         idColumn.setCellValueFactory(new PropertyValueFactory<Products, Integer>("id"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<Products, String>("name"));
         amountColumn.setCellValueFactory(new PropertyValueFactory<Products, Double>("amount"));
         priceColumn.setCellValueFactory(new PropertyValueFactory<Products, Double>("price"));
         supplierColumn.setCellValueFactory(new PropertyValueFactory<Products, String>("supplier"));
 
-        //кнопка добавить
-        add.setOnAction(event -> {
-            Products products = new Products(Integer.parseInt(inputId.getText()), inputName.getText(),
-                    Double.parseDouble(inputAmount.getText()), Double.parseDouble(inputPrice.getText()), inputSupplier.getText());
-            ObservableList<Products> products1 = productsTable.getItems();
-            products1.add(products);
-            productsTable.setItems(products1);
-        });
 
-        //кнопка удалить
-        remove.setOnAction(event -> {
-            int selectedID = productsTable.getSelectionModel().getSelectedIndex();
-            productsTable.getItems().remove(selectedID);
-        });
     }
 
 }

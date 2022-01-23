212ca0bb3895b35e76f52dbe42453005c599dda8
Svalyan <you@example.com>
2022-01-23 14:43:44 +0600

    new

--- html_css_js/Backend_study/index.js
+++ html_css_js/Backend_study/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import router from "./router.js";
+import fileUpload from 'express-fileupload'
 const PORT = 5000;
 const DB_URL = 'mongodb+srv://user:user@cluster0.y5eah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 const app = express();
 
 app.use(express.json())
+app.use(fileUpload({}))
 app.use('/api', router)
 
 async function startApp() {
